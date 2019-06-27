65dc2a7ef90db548d595d0f0cf58dd6fac16159f
mrbrownstone07 <mahdi.mad07@gmail.com>
2019-06-28 04:38:00 +0600

    updated dp, lcs code

--- dp/lcs.cpp
+++ dp/lcs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int max(int a, int b){
@@ -13,17 +14,36 @@ void lcs(string s1, string s2, string &R, int &len){
     s2Len = s2.length();
 
     int map[s1Len + 1] [s2Len + 1];
+        
     string temp = "";
 
     for(int i = 0; i <= s1Len; i++)
-        for(int j = 0; j <= s2Len; j++)
+        for(int j = 0; j <= s2Len; j++){
             if(i == 0 || j == 0) map[i][j] = 0;
             else if(s1[i-1] == s2[j-1]) {
                 map[i][j] = map[i-1][j-1] + 1;
                 temp += s2[j-1];
+                cout << s2[j-1] << endl;
             }
             else map[i][j] = max(map[i-1][j], map[i][j-1]);
+        }
 
+    stack <char> out;
+    int i = map[s1Len][s2Len];
+    for(int r = s1Len, c = s2Len; r >= 0 && c >= 0; ){
+        if(s1[r - 1] == s2[c-1]){
+            out.push(s1[r-1]);
+            r--;
+            c--;
+        }
+        else map[r-1][c] > map[r][c-1] ? r-- : c--;
+    }
+
+    while (!out.empty()){
+        cout << out.top();
+        out.pop();
+    }cout << endl;
+     
     len = map[s1Len][s2Len];
     R = temp; 
 }
